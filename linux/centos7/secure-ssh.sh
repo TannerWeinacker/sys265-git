820716ea6789341158666699c64fe093ea5fac29
TannerWeinacker <tanner.weinacker@mymail.champlain.edu>
2021-03-10 20:24:24 -0500

    centos7 secure ssh script

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#Secure-ssh.sh
+#Author Tanner Weinacker
+#Creates a new SSH user using $1 Parameter
+#Adds a public key from the local repo or culred from the remote repo
+# Removes roots ability to ssh in
+echo "All your code goes here"
